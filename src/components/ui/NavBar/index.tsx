515d30d4f09ada03911782bf4f236f81615ed279
assasinooz77 <assasinooz77@gmail.com>
2024-07-26 22:15:57 +0800

    feat: update mobile responsive ui for header

--- src/components/ui/NavBar/index.tsx
+++ src/components/ui/NavBar/index.tsx
@@ -5,16 +5,20 @@ import Link from "next/link";
 import { PAGE_LINKS } from "./links";
 import NavLink from "./NavLink";
 import { ConnectButton } from "~/components/common";
+import { useDisclosure } from "@mantine/hooks";
+import { NavBarModal } from "./NavBarModal";
 
 export const NavBar = () => {
+  const [opened, { open, close }] = useDisclosure(false);
+
   return (
     <header className="w-full">
-      <nav className="flex items-center justify-between gap-4 p-8">
+      <nav className="flex items-center justify-between gap-4 py-8 px-4 md:px-8">
         <Link href="/" className="flex items-center">
           <Image src="/img/logo.svg" width={129} height={56} alt="logo" />
         </Link>
 
-        <ul className="flex items-center gap-4">
+        <ul className="items-center gap-4 hidden lg:flex">
           {PAGE_LINKS.map((link, key) => (
             <NavLink
               icon={link.icon}
@@ -25,8 +29,20 @@ export const NavBar = () => {
           ))}
         </ul>
 
-        <ConnectButton />
+        <div className="flex items-center gap-4">
+          <ConnectButton />
+          <button onClick={open} className="block lg:hidden">
+            <Image
+              src="/img/icons/hamburger.svg"
+              width={40}
+              height={40}
+              alt=""
+            />
+          </button>
+        </div>
       </nav>
+
+      <NavBarModal opened={opened} onClose={close} />
     </header>
   );
 };
