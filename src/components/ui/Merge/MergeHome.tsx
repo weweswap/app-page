73d6c08fd66fff9a14a4d635aa2fe02215c0b9c1
ehsan <ehsan.saradar@gmail.com>
2024-08-02 22:52:43 +0330

    Update merge tab content

--- src/components/ui/Merge/MergeHome.tsx
+++ src/components/ui/Merge/MergeHome.tsx
@@ -10,7 +10,7 @@ export const MergeHome = () => {
       <Card>
         <div className="flex items-center justify-between gap-3">
           <Text size="xl" className="uppercase">
-            MERGE YOUR COINS
+            MERGE NOW
           </Text>
           <Text size="xl">
             🔥 🔥 🔥 
@@ -76,7 +76,7 @@ export const MergeHome = () => {
          
         </Text>
 
-        <ul className="list-decimal list-inside pt-3 dogica text-sm">
+        <ul className="list-decimal list-inside pt-3 verdana text-sm">
           <li>
             MERGE your WEWE into VULT
           </li>
