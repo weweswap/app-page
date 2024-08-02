4e0bf9b92d28ebce0de613c04187ea88b293e1b9
ehsan-saradar <ehsan.saradar@gmail.com>
2024-08-02 14:54:37 +0330

    add footer animation

--- src/components/ui/Footer/index.tsx
+++ src/components/ui/Footer/index.tsx
@@ -1,10 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
 
 export const Footer = () => {
+  const path = usePathname()
   return (
     <div className="flex flex-col items-center gap-9 p-4">
-      <Image src="/img/footer-logo.svg" width={400} height={60} alt="" />
+      {path === "/merge" ?
+        (
+          <video style={{ width: "500px" }} autoPlay loop muted>
+            <source src={"/videos/burn-animation.mov"} type="video/mp4"></source>
+          </video>
+        ) :
+        (
+          <Image src="/img/footer-logo.svg" width={400} height={60} alt="" />
+        )
+      }
       <div className="flex items-center gap-6">
         <Link href="https://x.com/weweswap" target="_blank">
           <Image src="/img/socials/x.svg" width={40} height={40} alt="" />
