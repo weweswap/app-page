f691f5c00eb76715f2868f686ef0b661bd912f16
Ehsan-saradar <ehsan.saradar@gmail.com>
2024-08-05 21:23:58 +0330

    Add open graph tags (#33)
    * update wewe-thumbnail pic
    
    * Add meta tags
    
    * Squashed commit of the following:
    
    commit 91c7618ad40863a63f29efb3bb9cfb0ad805e9ab
    Merge: 11ede98 3910480
    Author: xBi0 <saloiomisto@gmail.com>
    Date:   Mon Aug 5 14:33:31 2024 +0100
    
        Merge pull request #32 from weweswap/feat/ui-update
    
        Merge UI Update
    
    commit 39104806ccce60099174b012383318a4282e087c
    Author: assasinooz77 <assasinooz77@gmail.com>
    Date:   Mon Aug 5 21:19:12 2024 +0800
    
        feat: update mobile ui for merge page
    
    commit c877ed88d92293627bdac5913d6812b0d37916dc
    Author: assasinooz77 <assasinooz77@gmail.com>
    Date:   Mon Aug 5 21:15:01 2024 +0800
    
        feat: update footer animation size
    
    * Revert "Squashed commit of the following:"
    
    This reverts commit 9f919d1d2a2be650c0b367dabe211e5841e7ff14.
    
    * Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,12 +6,26 @@ const Document = () => {
     <Html lang="en">
       <Head>
         <link rel="apple-touch-icon" sizes="180x180" href="/fav.svg" />
-        <link rel="icon" type="image/svg" sizes="32x32" href="/fav.svg" />
-        <link rel="icon" type="image/svg" sizes="16x16" href="/fav.svg" />
+        <link rel="icon" type="image/svg+xml" sizes="32x32" href="/fav.svg" />
+        <link rel="icon" type="image/svg+xml" sizes="16x16" href="/fav.svg" />
         <link rel="manifest" href="/site.webmanifest" />
         <meta name="msapplication-TileColor" content="#000" />
         <meta name="theme-color" content="#000" />
         <title>WEWE Swap</title>
+        <meta name="description" content="Click MERGE to burn your $WEWE and receive $VULT. Starting price is 1,000 $WEWE to 1 $VULT, but this will rise. Your $VULT will be locked until the public launch." />
+        <meta name="author" content="wewe swap" />
+        <meta name="keywords" content="WeWe app,WeWe Swap,WeWe Token,DeFi,MEME,MEME Coin,DeFi,USDC Yield,Yields,Earnings,Chaos,Pools" />
+        <meta property="og:title" content="WEWESwap - Together We Earn " />
+        <meta property="og:description" content="We Love Yields!" />
+        <meta property="og:url" content="https://app.weweswap.com" />
+        <meta property="og:site_name" content="WEWE Swap" />
+        <meta property="og:image" content="https://app.weweswap.com/img/wewe-thumbnail.png" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="app.weweswap.com" />
+        <meta name="twitter:title" content="WEWE - Together We Earn" />
+        <meta name="twitter:description" content="We Love Yields!" />
+        <meta name="twitter:image" content="https://app.weweswap.com/img/wewe-thumbnail.png" />
       </Head>
       <body className={dogica.className}>
         <Main />
