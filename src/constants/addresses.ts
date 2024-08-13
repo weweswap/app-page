f274232a2b31886481e810fe0ae8b1eb488be261
assasinooz77 <assasinooz77@gmail.com>
2024-08-13 17:50:31 +0800

    feat: update contract address

--- src/constants/addresses.ts
+++ src/constants/addresses.ts
@@ -1,5 +1,5 @@
 export const CONTRACT_ADDRESSES = {
   wewe: "0x6b9bb36519538e0C073894E964E90172E1c0B41F" as `0x${string}`,
-  vult: "0xaB25B6a2a93147f2723e5a3F6Eb3BC663333801B" as `0x${string}`,
-  merge: "0xa68076E046Fd576B5BE364daf1f00347Bf6f79D9" as `0x${string}`,
+  vult: "0x299A57C1f6761b3dB304dc8B18bb4E60A1CF37b6" as `0x${string}`,
+  merge: "0x30091c97Fd47873c44D03F7F1b960473D300D269" as `0x${string}`,
 };
