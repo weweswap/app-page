e9cda9a7adb2e21d90958ec5f44da1873e3ad02e
Rohan Kumar Pandey <rohan@Rohans-MacBook-Air.local>
2024-09-02 16:06:07 +0530

    minor changes as per figma

--- src/components/ui/Migrate/MigrateDetail.tsx
+++ src/components/ui/Migrate/MigrateDetail.tsx
@@ -120,7 +120,7 @@ export const MigrateDetail = ({ onBack, onMigrate }: MigrateDetailProps) => {
             <Typography size="sm">NARROW 40%</Typography>
           </button>
         </div> */}
-         <Button onClick={onMigrate} className="outline px-10 my-5 w-full">
+         <Button disabled onClick={onMigrate} className="px-10 my-5 w-full">
         <Typography secondary size="xs" fw={700}>
           MIGRATE
         </Typography>
@@ -135,7 +135,11 @@ export const MigrateDetail = ({ onBack, onMigrate }: MigrateDetailProps) => {
               <Typography size="xs" fw={700}>
                 0.001079432
               </Typography>
-              <Image src="/img/tokens/weth.png" width={24} height={24} alt="" />
+              <div className="flex items-center gap-2">
+              <Image src="/img/tokens/eth.png" width={24} height={24} alt="" />
+              <Image src="/img/tokens/wewe.svg" width={24} height={24} alt="" />
+              </div>
+              
             </div>
           </div>
 
@@ -145,7 +149,6 @@ export const MigrateDetail = ({ onBack, onMigrate }: MigrateDetailProps) => {
               <Typography size="xs" fw={700}>
                 0.428156
               </Typography>
-              <Image src="/img/tokens/wewe.png" width={24} height={24} alt="" />
             </div>
           </div>
 
@@ -153,50 +156,82 @@ export const MigrateDetail = ({ onBack, onMigrate }: MigrateDetailProps) => {
             <Typography size="xs">INITIAL WEWE</Typography>
             <div className="flex items-center gap-2">
               <Typography size="xs" fw={700}>
-                0.001079432
+                0.428156
               </Typography>
-              <Image src="/img/tokens/usdc.png" width={24} height={24} alt="" />
             </div>
           </div>
 
+          <div className="flex items-center justify-between py-4">
+            <Typography size="xs">POOL TYPE</Typography>
+            <div className="flex items-center gap-2">
+              <div className="flex items-center gap-8">
+                <div className="flex items-center gap-2">
+              <Image src="/img/icons/rocket.svg" width={24} height={24} alt="" />
+                <Typography size="xs" fw={700}>
+                MEMES 1%
+              </Typography>
+                </div>  
+                <div className="flex items-center gap-2">
+              <Image src="/img/icons/Infinity.svg" width={24} height={24} alt="" />
+                <Typography size="xs" fw={700}>
+               INFINITY
+              </Typography>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <Typography size="xs">RATE</Typography>
+            <div className="flex items-center gap-2">
+              <Typography size="xs" fw={700}>
+                1 USDC = 1000 WEWE ($1.00)
+              </Typography>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <Typography size="xs">RANGE</Typography>
+            <div className="flex items-center gap-2">
+              <Typography size="xs" fw={700}>
+                Min. 0 - Max. 999999+
+              </Typography>
+              <Image src="/img/tokens/weth.png" width={24} height={24} alt="" />
+            </div>
+          </div>
           <div className="flex items-center justify-between">
             <Typography size="xs">END USDC</Typography>
             <div className="flex items-center gap-2">
               <Typography size="xs" fw={700}>
-                0.001079432
+                20.00
               </Typography>
               <Image src="/img/tokens/weth.png" width={24} height={24} alt="" />
             </div>
           </div>
+
           <div className="flex items-center justify-between">
             <Typography size="xs">END WEWE</Typography>
             <div className="flex items-center gap-2">
               <Typography size="xs" fw={700}>
-                0.001079432
+                1,000,000.00
               </Typography>
               <Image src="/img/tokens/weth.png" width={24} height={24} alt="" />
             </div>
           </div>
 
           <div className="flex items-center justify-between">
-            <Typography size="xs">NEW LP</Typography>
+            <Typography className="font-bold" size="xs">WEWE SHARES</Typography>
             <div className="flex items-center gap-2">
               <Typography size="xs" fw={700}>
                 0.001079432
               </Typography>
-              <Image src="/img/tokens/weth.png" width={24} height={24} alt="" />
+              <div className="flex items-center gap-2">
+              <Image src="/img/tokens/eth.png" width={24} height={24} alt="" />
+              <Image src="/img/tokens/wewe.svg" width={24} height={24} alt="" />
+              </div>
+              
             </div>
           </div>
-
-          <div className="flex items-center justify-between">
-            <Typography size="xs">Slippage 1.05%</Typography>
-            <div className="flex items-center gap-2">
-              <Typography size="xs" fw={700}>
-                Estimated amount: $0.017
-              </Typography>
-            </div>
-          </div>
-
           <div className="flex items-center justify-between">
             <Typography size="xs">Slippage 1.05%</Typography>
             <div className="flex items-center gap-2">
