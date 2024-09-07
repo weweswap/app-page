b84290b5ab9082f0160a4283af1fd1ea7cc3b3fe
Ehsan-saradar <ehsan.saradar@gmail.com>
2024-09-07 18:15:30 +0330

    minor fix

--- src/components/ui/Pool/PoolBox.tsx
+++ src/components/ui/Pool/PoolBox.tsx
@@ -8,9 +8,8 @@ const PoolBox = () => {
   return (
     <>
     <div className='w-full flex flex-col gap-6'>
-        {DUMMY_POOLS.map(({title, exchangePair, state, range, lpValue, rewards, positionId}) => {
-            return  <div key={title} className='bg_dark w-full min-h-[10rem] p-4'>
-            <Typography>{title}</Typography> 
+        {DUMMY_POOLS.map(({ exchangePair, state, range, lpValue, rewards, positionId}) => {
+            return  <div className='bg_dark w-full min-h-[10rem] p-4'> 
             <div className='sm:py-4 py-7 flex items-center justify-between gap-3 flex-wrap'>
              <div className='flex items-center gap-2'>
              <div className="flex items-center">
