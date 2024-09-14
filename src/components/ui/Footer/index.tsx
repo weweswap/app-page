3157af381eb072cf494da3ce64e79b541ed9884d
Ehsan-saradar <ehsan.saradar@gmail.com>
2024-09-14 18:04:05 +0330

    Force rebuild

--- src/components/ui/Footer/index.tsx
+++ src/components/ui/Footer/index.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Lottie from "lottie-react";
 import animation from "../../../../public/videos/burn-animation.json";
-
 export const Footer = () => {
   const path = usePathname();
   return (
