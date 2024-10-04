5149d85661a1aaf98dde39526fc9ba7ada1a7081
mohammad reza ebrhimi <mr.ebrahimi96@gmail.com>
2024-10-04 13:58:41 +0200

    fix: change process modal text

--- src/components/ui/Merge/Bro/MergeProcessingModal.tsx
+++ src/components/ui/Merge/Bro/MergeProcessingModal.tsx
@@ -10,23 +10,23 @@ import { TokenItem } from '~/models';
 import { Hex } from 'viem';
 
 export type PayloadMergeProcessingModald = {
-    amountToMerge: string,
-    token: TokenItem
-    eater: Hex
+  amountToMerge: string,
+  token: TokenItem
+  eater: Hex
 }
 
 type MergeProcessingModalProps = {
-    onClose: () => void;
-    onOpen: () => void;
-    onTxError: (hash?: Hex) => void;
-    onMergeSuccess: (hash?: Hex) => void;
-    opened: boolean;
-    data: PayloadMergeProcessingModald
-  } & ModalRootProps;
+  onClose: () => void;
+  onOpen: () => void;
+  onTxError: (hash?: Hex) => void;
+  onMergeSuccess: (hash?: Hex) => void;
+  opened: boolean;
+  data: PayloadMergeProcessingModald
+} & ModalRootProps;
 
-const MergeProcessingModal = ({ data, onClose, onTxError, onMergeSuccess, opened}: MergeProcessingModalProps) => {
+const MergeProcessingModal = ({ data, onClose, onTxError, onMergeSuccess, opened }: MergeProcessingModalProps) => {
   const { address } = useAccount();
-  
+
   const {
     hash: hashApproveBroToken,
     isPending: isPendingApproveBroToken,
@@ -44,7 +44,7 @@ const MergeProcessingModal = ({ data, onClose, onTxError, onMergeSuccess, opened
   } = useEat(data.eater);
 
   useEffect(() => {
-    async function startEat () {
+    async function startEat() {
       await approveBroToken(data.token.address, data.eater, BigInt(data.amountToMerge || '0'))
       await eat(data.amountToMerge)
     }
@@ -68,45 +68,45 @@ const MergeProcessingModal = ({ data, onClose, onTxError, onMergeSuccess, opened
   return (
     <Modal title="MERGE TOKENS" onClose={onClose} opened={opened}>
       <div className='flex flex-col gap-3'>
-          <div className='flex gap-3 items-center'>
-              { isPendingApproveBroToken || isConfirmingApproveBroToken || !hashApproveBroToken 
-                  ? <>
-                    <Loader color="grey" />
-                    <Typography>Please Approve {data.token.symbol}</Typography>
-                  </>
-                  : <>
-                    <Image src="/img/icons/success.svg" width={36} height={36} alt='' />
-                    <Typography>{data.token.symbol} Approved</Typography>
-                  </>
-              }
-          </div>
-          <div className='flex gap-3 items-center'>
-              { isPendingEatBroToken || !hashEatBroToken 
-                  ? <>
-                    <Loader color="grey" />
-                    <Typography>Please deposit tokens</Typography>
-                  </>
-                  : <>
-                    <Image src="/img/icons/success.svg" width={36} height={36} alt='' />
-                    <Typography>Tokens deposited</Typography>
-                  </>
-              }
-          </div>
-          {
-            !isConfirmingApproveBroToken && !isConfirmingEatBroToken && !finishSuccessfully &&
-            <div className='flex gap-3 items-center'>
-              <Image src="/img/icons/inform.svg" width={36} height={36} alt='' />
-              <Typography>Please sign transaction</Typography>
-            </div>
+        <div className='flex gap-3 items-center'>
+          {isPendingApproveBroToken || isConfirmingApproveBroToken || !hashApproveBroToken
+            ? <>
+              <Loader color="grey" />
+              <Typography>Please Approve {data.token.symbol}</Typography>
+            </>
+            : <>
+              <Image src="/img/icons/success.svg" width={36} height={36} alt='' />
+              <Typography>{data.token.symbol} Approved</Typography>
+            </>
           }
+        </div>
+        <div className='flex gap-3 items-center'>
+          {isPendingEatBroToken || !hashEatBroToken
+            ? <>
+              <Loader color="grey" />
+              <Typography>Please merge tokens</Typography>
+            </>
+            : <>
+              <Image src="/img/icons/success.svg" width={36} height={36} alt='' />
+              <Typography>Tokens merged</Typography>
+            </>
+          }
+        </div>
+        {
+          !isConfirmingApproveBroToken && !isConfirmingEatBroToken && !finishSuccessfully &&
+          <div className='flex gap-3 items-center'>
+            <Image src="/img/icons/inform.svg" width={36} height={36} alt='' />
+            <Typography>Please sign transaction</Typography>
+          </div>
+        }
       </div>
       <Divider className="border-blue-700" />
       <div className='flex justify-end'>
-          <Typography className='text_light_gray' size='xs'>
-              Total fee cost: $0.10
-          </Typography>
+        <Typography className='text_light_gray' size='xs'>
+          Total fee cost: $0.10
+        </Typography>
       </div>
-  </Modal>
+    </Modal>
   )
 }
 
