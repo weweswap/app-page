592ece132e3c65a63c68e83e3c5aab8627dae824
Rohan Kumar Pandey <rohanpandey749@gmail.com>
2024-10-30 11:24:14 +0530

    added gtm to the weweswap

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,18 @@ const Document = () => {
   return (
     <Html lang="en">
       <Head>
+         {/* Google Tag Manager Script */}
+         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-WC9Q45VH');
+            `,
+          }}
+        />
         <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="/fav.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/fav.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/fav.png" />
@@ -45,6 +57,14 @@ const Document = () => {
         />
       </Head>
       <body className={verdana.className}>
+      <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-WC9Q45VH"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
         <Main />
         <NextScript />
       </body>
