9165eb9a167b1458b416bbb6a1a095a01da665cc
mohammad reza ebrhimi <mr.ebrahimi96@gmail.com>
2024-11-26 10:47:16 +0100

    chore: update the pools and show new one

--- src/constants/poolsConfigs.ts
+++ src/constants/poolsConfigs.ts
@@ -5,7 +5,11 @@ interface PoolStaticDetail {
   range: string;
 }
 
-export const POOLS_BLACKLIST = ["0xC3b2FfDE3403Fb9461F2690A72Fa701aE58140Bd"].map(address => address.toLowerCase());
+export const POOLS_BLACKLIST = [
+  "0xC3b2FfDE3403Fb9461F2690A72Fa701aE58140Bd", 
+  "0xA1dD90A50e423c7D7FEC1408e867606494BcDF2e", 
+  "0x137c8040d44e25D2c7677224165Da6Aa0901e33B"
+].map(address => address.toLowerCase());
 
 export const PoolsStaticDetail = [
   {
@@ -13,11 +17,11 @@ export const PoolsStaticDetail = [
     range: "INFINITY",
   },
   {
-    address: "0x137c8040d44e25D2c7677224165Da6Aa0901e33B",
+    address: "0xf5E50FA50a9053a03f8dF54bEaA56f1E4870B569",
     range: "0.1%"
   },
   {
-    address: "0xA1dD90A50e423c7D7FEC1408e867606494BcDF2e",
+    address: "0xb1c7a879de8dd961526433525b85587dcF4BD6ff",
     range: "INFINITY"
   }
 ]
